268068fc02320a63f0b9b7e1c7501d099fe80f73
Michael Wong <mik9080@gmail.com>
2017-09-27 15:44:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeANumber(katzDeliLine,name){
   for (var i=0;i<katzDeliLine.length;i++){
     if(katzDeliLine[i]==name){
-      return i+1
+      return `Welcome, ${name}. You are number ${i+1} in line.`
     }
   }
 }
